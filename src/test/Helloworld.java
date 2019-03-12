1db486453fab5e0de7963ca396aa0ca48baca300
yanzi99522 <907591384@qq.com>
2019-03-12 12:50:29 +0800

    helloworld

--- src/test/Helloworld.java
+++ src/test/Helloworld.java
@@ -2,6 +2,7 @@ package test;
 
 public class Helloworld {
     public static void main(String[] args) {
+        System.out.println("today is 20190312");
         System.out.println("hello world ,this is a test!");
     }
 }
