72a05af2836eed85cdb7e90f0b702195c503cd46
yanzi99522 <907591384@qq.com>
2019-03-14 21:32:12 +0800

    文件上传--网络编程01

---
+++ src/network/UploadClient.java
@@ -0,0 +1,64 @@
+/*
+* Created by �ǽݿ���
+* ������վ��http://www.zhijieketang.com/group/5
+* �ǽݿ������߿��ã�www.zhijieketang.com
+* �ǽݿ���΢�Ź����ţ�zhijieketang
+* ���䣺eorient@sina.com
+* Java���߷���QQȺ��547370999
+*
+* ��Java��С�׵���ţ��ֽ�ʰ�ͼ�飬��������Ƶ
+*
+* �����׵����顿��ַ��
+*       ͼ��������
+*       http://www.ituring.com.cn/book/2480
+*       �ٶ��Ķ���
+*       https://yuedu.baidu.com/ebook/7c1499987e192279168884868762caaedd33ba00 
+*/
+
+
+//UploadClient.java�ļ�
+package network;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.Socket;
+
+public class UploadClient {
+
+	public static void main(String[] args) {
+
+		System.out.println("客户端运行...");
+
+		try ( // 向本机的8080端口发出请求
+				Socket socket = new Socket("127.0.0.1", 8080);
+				// 由Socket获得输出流，并创建缓冲输出流
+				BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
+				// 创建文件输入流
+				FileInputStream fin = new FileInputStream("C:\\Users\\lxy\\Pictures\\cos.jpg");
+				//由文件输入流创建缓冲输入流
+				BufferedInputStream in = new BufferedInputStream(fin)) {
+
+			// 准备一个缓冲区
+			byte[] buffer = new byte[1024];
+			//首次读取文件
+			int len = in.read(buffer);
+			while (len != -1) {
+				/// 数据写入Socket
+				out.write(buffer, 0, len);
+				// 再次读取文件
+				len = in.read(buffer);
+			}
+
+			System.out.println("上传成功！");
+
+		} catch (ConnectException e) {
+			System.out.println("服务器未启动");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/network/UploadServer.java
@@ -0,0 +1,41 @@
+package network;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class UploadServer {
+
+	public static void main(String[] args) {
+
+		System.out.println("服务器端运行...");
+
+		try ( // 创建一个ServerSocket监听8080端口的客户端请求
+				ServerSocket server = new ServerSocket(8080);
+				// 使用accep（）阻塞当前线程，等待客户端请求
+				Socket socket = server.accept();
+				//由Socket获得输入流，并创建缓存输入流
+				BufferedInputStream in = new BufferedInputStream(socket.getInputStream());
+				// 由文件输出流创建缓冲输出流
+				FileOutputStream out = new FileOutputStream("C:\\Users\\lxy\\Pictures\\cos.jpg")) {
+
+			// 准备一个缓冲区
+			byte[] buffer = new byte[1024];
+			// 首次从Socket读取数据
+			int len = in.read(buffer);
+			while (len != -1) {
+				// 写入数据到文件
+				out.write(buffer, 0, len);
+				// 再次从Socket读取数据
+				len = in.read(buffer);
+			}
+
+			System.out.println("接收完成");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
