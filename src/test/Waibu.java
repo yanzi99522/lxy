e9bf16009ff210be5d3c27814b5223aa7ff2c4b2
yanzi99522 <907591384@qq.com>
2019-03-16 22:09:21 +0800

    count

--- src/test/Waibu.java
+++ src/test/Waibu.java
@@ -3,7 +3,6 @@ package test;
 public class Waibu {
 
     public static void main(String[] args) {
-        // TODO 自动生成的方法存根
         int[] numbers= {1,2,3,4,5,6,7,8,9,10};
         for(int i=0;i<numbers.length;i++) {
             if(i==3) {
