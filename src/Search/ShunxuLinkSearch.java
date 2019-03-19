a1fff7fa70c97cba2bcdf9c8cc776bfe34ab7627
yanzi99522 <907591384@qq.com>
2019-03-19 11:50:06 +0800

    顺序表结构查找

---
+++ src/Search/ShunxuLinkSearch.java
@@ -0,0 +1,99 @@
+package Search;
+
+import java.util.Scanner;
+
+public class ShunxuLinkSearch {
+    public static void main(String[] args) {
+        int i;
+        SLType SL=new SLType();//定义顺序表变量
+        Data pdata;//定义结点保存指针变量
+        Scanner input = new Scanner(System.in);
+        System.out.println("顺序表操作演示：");
+        SL.SLInit(SL);//初始化顺序表
+
+        System.out.println("初始化顺序表完成！");
+
+        do {
+            System.out.println("输入添加的结点（学号 姓名 年龄）：");
+            Data data=new Data();
+            data.key=input.next();
+            data.name=input.next();
+            data.age=input.nextInt();
+            if(data.age>0){//若年龄不为0
+                if(SL.SLAdd(SL,data)==0)//若添加结点失败
+                {
+                    break;
+                }
+            }
+            else {//若年龄为0
+                break;//退出死循环
+            }
+        }while (true);
+
+        System.out.println("顺序表中的结点顺序为：");
+        SL.SLAll(SL);//显示所有结点数据
+
+        System.out.println("要取出结点的序号：");
+        i=input.nextInt();//输入结占点序号
+        pdata=SL.SLFindByNum(SL,i);//按序号查找结点
+        if (!pdata.equals(null)){
+            System.out.print("第"+i+"个结点为：<"+pdata.key+" "+pdata.name+" "+pdata.age+">");
+        }
+    }
+}
+
+class Data{
+    String key;//结点的关键字
+    String name;
+    int age;
+}//定义结点类型
+
+class SLType{//定义顺序表结构
+    static final int MAXLEN=100;
+    Data[] ListData=new Data[MAXLEN+1];//保存顺序表的结构数组
+    int ListLen;//顺序表已存结点的数量
+
+    void SLInit(SLType SL){//初始化顺序表
+        SL.ListLen=0;//初始化为空表
+    }
+
+    int SLLength(SLType SL){
+        return SL.ListLen;//返回顺序表的元素数量
+    }
+
+    int SLAdd(SLType SL,Data data){//增加元素到顺序表尾部
+        if (SL.ListLen==MAXLEN){//顺序表已满
+            System.out.println("顺序表已满，不能再添加结点了！");
+            return 0;
+        }
+        SL.ListData[++SL.ListLen] = data;
+        return 1;
+    }
+
+    Data SLFindByNum(SLType SL,int n){//根据序号返回数据元素
+        if (n<1||n>SL.ListLen+1){
+            System.out.println("结点序号错误，不能返回结点！");//不成功，则返回0
+            return null;//不成功，则返回0
+        }
+        return SL.ListData[n];
+    }
+    int SLFindByCont(SLType SL,String key){//按关键字查询结点
+        int i;
+        for (i=1;i<=SL.ListLen;i++){
+            if (SL.ListData[i].key.compareTo(key)==0){//如果找到所需结点
+                return i;//返回结点序号
+            }
+        }
+        return 0;//搜索整个表后仍然没有找到，则返回0
+    }
+
+    int SLAll(SLType SL){//显示顺序表中的所有结点
+        int i;
+        for (i=1;i<SL.ListLen;i++){
+            System.out.println("<"+SL.ListData[i].key+" "+SL.ListData[i].name+" "+SL.ListData[i].age);
+        }
+        return 0;
+
+    }
+
+}
