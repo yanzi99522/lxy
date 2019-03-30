5c44483c394789894dfeca5efecf03cc96d9fe42
yanzi99522 <907591384@qq.com>
2019-03-30 19:31:23 +0800

    z

---
+++ src/hank/Solution4.java
@@ -0,0 +1,16 @@
+package hank;
+
+import java.util.Scanner;
+
+public class Solution4 {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in=new Scanner(System.in);
+
+       for (int i=1;in.hasNext()==true;i++){
+
+            System.out.println(i+" "+in.nextLine());
+        }
+    }
+}
