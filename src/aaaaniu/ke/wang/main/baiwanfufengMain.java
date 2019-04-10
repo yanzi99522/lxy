a1915a3b0bfdbb23624917e9d16929dc4b117312
yanzi99522 <907591384@qq.com>
2019-04-10 21:44:17 +0800

    百万富翁

---
+++ src/aaaaniu/ke/wang/main/baiwanfufengMain.java
@@ -0,0 +1,8 @@
+package aaaaniu.ke.wang.main;
+
+public class baiwanfufengMain {
+    public static void main(String[] args) {
+        System.out.println(30*10);
+        System.out.println((long)Math.pow(2,30)-1);
+    }
+}
