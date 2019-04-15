ea8cd438a1f6eef8e9ef05a1e90811a9bcf3b5ba
yanzi99522 <907591384@qq.com>
2019-04-15 19:26:54 +0800

    9*9

---
+++ src/aaaaniu/ke/wang/datastructure/jiujiu.java
@@ -0,0 +1,12 @@
+package aaaaniu.ke.wang.datastructure;
+
+public class jiujiu {
+    public static void main(String[] args) {
+        for (int i=1;i<10;i++){
+            for (int j=i;j<10;j++){
+                System.out.print(i+"*"+j+"="+i*j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
