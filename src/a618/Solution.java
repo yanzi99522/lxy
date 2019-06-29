fd76ed70b06cf9647b6a419b3e639d2b3ced2698
yanzi99522 <907591384@qq.com>
2019-06-29 21:07:40 +0800

    1的个数

---
+++ src/a618/Solution.java
@@ -0,0 +1,29 @@
+package a618;
+
+/**
+ * @author lxy
+ */
+public class Solution {
+    static int NumberOf1Between1AndN_Solution(int n) {
+        int count=0;
+        while(n>0){
+            String s=String.valueOf(n);
+            char[] chars=s.toCharArray();
+            for(int i=0;i<chars.length;i++){
+                if(chars[i]=='1'){
+                    count++;
+                }
+            }
+            n--;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Solution test=new Solution();
+       int a=NumberOf1Between1AndN_Solution(13);
+        System.out.println(a);
+
+    }
+}
