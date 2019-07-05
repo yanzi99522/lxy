a84103cb14864d9b6f8dfa9ed194c455127701cf
yanzi99522 <907591384@qq.com>
2019-07-05 22:07:23 +0800

    tree

---
+++ src/a0705/TREE.java
@@ -0,0 +1,18 @@
+package a0705;
+
+/**
+ * @author lxy
+ */
+ class TreeNode {
+    int val=0;
+   TreeNode left=null;
+    TreeNode right=null;
+
+    public TreeNode(int val){
+        this.val=val;
+    }
+    public class TREE{
+
+    }
+
+}
