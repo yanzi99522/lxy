1cccc95d773215b03f640df3622cd71abafe1d91
yanzi99522 <907591384@qq.com>
2019-07-14 22:03:51 +0800

    等差数列

---
+++ src/a0704/Main7.java
@@ -0,0 +1,17 @@
+package a0704;
+
+import java.util.Scanner;
+
+/**
+ * @author lxy
+ */
+public class Main7 {//等差数列
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int result=(3*n+1)*n/2;
+        System.out.println(result);
+    }
+
+}
